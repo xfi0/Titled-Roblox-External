df1f6c2880c4ca633a556afced3a4bbb200ac62b
dom <166765848+xfi0@users.noreply.github.com>
2025-11-05 17:35:42 -0600

    Fixed!

--- src/cache/cache.cpp
+++ src/cache/cache.cpp
@@ -4,36 +4,54 @@
 
 void cache::Run() {
 	while (true) {
-		std::vector<Roblox::player_t> players = game::players.GetChildren<Roblox::player_t>();
+		try {
+			std::vector<Roblox::player_t> players = game::players.GetChildren<Roblox::player_t>();
 
-		std::vector<cache::entity_t> tempCache;
-		for (Roblox::player_t& player : players) {
-			cache::entity_t entity{};
+			std::vector<cache::entity_t> tempCache;
+			for (Roblox::player_t& player : players) {
+				cache::entity_t entity{};
 
-			entity.name = player.GetName();
-			//entity.rigTsype = 
+				entity.name = player.GetName();
+				//entity.rigTsype = 
 
-			Roblox::model_instance_t modelInstance = player.GetModelInstance();
+				Roblox::model_instance_t modelInstance = player.GetModelInstance();
 
-			auto parts = modelInstance.GetChildren<Roblox::part_t>();
-			for (auto& part : parts) {
-				std::string partClass = part.GetClassNameA();
-				if (partClass.find("part") != std::string::npos) {
-					entity.parts[part.GetName()] = part;
+				auto parts = modelInstance.GetChildren<Roblox::part_t>();
+				for (auto& part : parts) {
+					std::string partClass = part.GetClassNameA();
+					if (partClass.find("part") != std::string::npos) {
+						entity.parts[part.GetName()] = part;
+					}
 				}
+
+				entity.humanoid = modelInstance.FindFirstChild("Humanoid").address;
+				entity.rigType = entity.humanoid.GetRigType();
+				entity.humanoid.Health = memory->read<float>(entity.humanoid.address + Offsets::Humanoid::Health);
+				entity.humanoid.MaxHealth = memory->read<float>(entity.humanoid.address + Offsets::Humanoid::MaxHealth);
+
+				tempCache.push_back(entity);
+				printf("\n%s ", game::players.GetClassNameA().c_str());
+				Roblox::instance_t inst = game::players.FindFirstChildByClass("Player");
+				cachedLocalPlayer = {}; // clear
+				if (inst.address) {
+					for (auto& ent : tempCache) {
+						if (ent.humanoid.address && ent.humanoid.address == inst.address) {
+							cachedLocalPlayer = ent;
+							break;
+						}
+						if (!ent.name.empty() && ent.name == inst.GetName()) { // fallback
+							cachedLocalPlayer = ent;
+							break;
+						}
+					}
+				}
+
+				cachedPlayers = tempCache;
 			}
-
-			entity.humanoid = modelInstance.FindFirstChild("Humanoid").address;
-			entity.rigType = entity.humanoid.GetRigType();
-			entity.humanoid.Health = memory->read<float>(entity.humanoid.address + Offsets::Humanoid::Health);
-			entity.humanoid.MaxHealth = memory->read<float>(entity.humanoid.address + Offsets::Humanoid::MaxHealth);
-
-			tempCache.push_back(entity);
-
-			//cachedLocalPlayer = memory->read<entity_t>(game::players.address + Offsets::Player::LocalPlayer); // might work
-			cachedPlayers = tempCache;
 		}
+		catch (...) {
 
+		}
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
 }
